c54ae369e3ab0726c0aeeaefc231a4208c667b70
ISHaNT-CHaHaR <56401920+ISHaNT-CHaHaR@users.noreply.github.com>
2020-03-17 14:48:33 +0530

    added validity feature.
    .

--- main.js
+++ main.js
@@ -37,10 +37,29 @@ class Blockchain {
       newBlock.hash = newBlock.calculateHash();
       this.chain.push(newBlock);
    }
+
+   isChainValid() {
+      for (let i = 1; i < this.chain.length; i++) {
+         const currentBlock = this.chain[i];
+         const previousBlock = this.chain[i - 1];
+
+         if (currentBlock.hash !== currentBlock.calculateHash()) {
+            return false;
+         }
+         if (currentBlock.previousHash !== previousBlock.hash) {
+            return false;
+         }
+      }
+      return true;
+   }
 }
 
 let coin = new Blockchain();
 coin.addBlock(new Block(1, '17/3/20', { amount: 4 }));
 coin.addBlock(new Block(2, '17/3/20', { amount: 4 }));
 
-console.log(JSON.stringify(coin, null, 6));
+console.log('is Blockchain validity? ' + coin.isChainValid());
+
+coin.chain[1].data = { amount: 100 };
+
+console.log('is Blockchain validity? ' + coin.isChainValid());
