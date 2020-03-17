314a31bb43d06950f19c89e3d5a9644db6278d9c
ISHaNT-CHaHaR <56401920+ISHaNT-CHaHaR@users.noreply.github.com>
2020-03-17 14:19:08 +0530

    created block and chain classes
    .

--- main.js
+++ main.js
@@ -1,1 +1,46 @@
+const SHA256 = require('index.js');
 
+class Block {
+   constructor(index, timestamp, data, previousHash = '') {
+      this.index = index;
+      this.timestamp = timestamp;
+      this.data = data;
+      this.previousHash = previousHash;
+      this.hash = this.calculateHash;
+   }
+
+   calculateHash() {
+      return SHA256(
+         this.index +
+            this.previousHash +
+            this.timestamp +
+            JSON.stringify(this.data)
+      ).tostring();
+   }
+}
+
+class Blockchain {
+   constructor() {
+      this.chain = [this.craeateBlock()];
+   }
+
+   craeateBlock() {
+      return new Block(0, 17 / 3 / 2020, 'MY bLOCK', '0');
+   }
+
+   getLatestBlock() {
+      return this.chain(this.chain.length - 1);
+   }
+
+   addBlock(newBlock) {
+      newBlock.previousHash = this.getLatestBlock().hash;
+      newBlock.hash = newBlock.calculateHash();
+      this.chain.push(newBlock);
+   }
+}
+
+let coin = new Blockchain();
+coin.addBlock(new Block(1, '17/3/20', { amount: 4 }));
+coin.addBlock(new Block(2, '17/3/20', { amount: 4 }));
+
+console.log(JSON.stringify(coin, null, 6));
